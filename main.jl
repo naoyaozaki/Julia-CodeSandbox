1318b365f221cc900850625fa87dc44a6c12df16
Naoya <naoya.ozaki11@gmail.com>
2023-02-08 14:30:00 +0000

    Update Dockerfile and main.jl

--- main.jl
+++ main.jl
@@ -1,2 +1,14 @@
-import JuMP
-println("Hello World!")
+using JuMP
+import Ipopt
+
+function example_rosenbrock()
+    model = Model(Ipopt.Optimizer)
+    # set_silent(model)
+    @variable(model, x)
+    @variable(model, y)
+    @NLobjective(model, Min, (1 - x)^2 + 100 * (y - x^2)^2)
+    optimize!(model)
+    return
+end
+
+example_rosenbrock()
